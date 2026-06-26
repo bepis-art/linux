d3259e45ae04254758240ab4e151595261976971
bepis-art <gulamovart@gmail.com>
2026-06-26 10:55:56 +0500

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -2,15 +2,14 @@
 #Скрипт для установки Docker, Docker Compose и установления всех зависимостей
 
 # Установка дополнительных плагинов
-apt update  
-apt upgrade
+apt update && apt upgrade
 apt install ca-certificates gnupg curl
 
 # Установка Docker
 # GPG-ключ, который будет использоваться пакетным менеджером apt для проверки подлинности пакетов из репозитория Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update
 apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin
